457d46bd1c09555ed827e96675463d24d7641045
Stoungi <36931144+Stoungi@users.noreply.github.com>
2025-03-18 15:30:38 +0000

    Update pivot.py
    Just to show you do not need to put the tags in a specific way

--- pivot.py
+++ pivot.py
@@ -7,6 +7,6 @@ file = Process('large_pivot_table_data.xlsx')
 
 file.input(**{"Region" : ["North"], "Chart" : ["bar", "line", "pie"], "save as": "just_north.pdf"})
 
-file.input(**{"Region" : ["North", "East"], "Chart" : "*", "save as": "north_vs_west.pdf"})
+file.input(**{"Chart" : "*", "Region" : ["North", "East"], "save as": "north_vs_west.pdf"})
 
 file.input()
